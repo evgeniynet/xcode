e2c018e5f801a34f1f7efb88c812749058508bbc
Eugene <jtrue@mail.ru>
2019-05-10 08:58:38 +0300

    connectivity

---
+++ Sherpadesk/Classes/ConnectivityHandler.swift
@@ -0,0 +1,42 @@
+import Foundation
+import WatchConnectivity
+
+class ConnectivityHandler : NSObject, WCSessionDelegate {
+    
+    var session = WCSession.default()
+    
+    override init() {
+        super.init()
+        
+        session.delegate = self
+        session.activate()
+        
+        NSLog("%@", "Paired Watch: \(session.isPaired), Watch App Installed: \(session.isWatchAppInstalled)")
+    }
+    
+    // MARK: - WCSessionDelegate
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        NSLog("%@", "activationDidCompleteWith activationState:\(activationState) error:\(error)")
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        NSLog("%@", "sessionDidBecomeInactive: \(session)")
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        NSLog("%@", "sessionDidDeactivate: \(session)")
+    }
+    
+    func sessionWatchStateDidChange(_ session: WCSession) {
+        NSLog("%@", "sessionWatchStateDidChange: \(session)")
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        NSLog("didReceiveMessage: %@", message)
+        if message["request"] as? String == "date" {
+            replyHandler(["date" : "\(Date())"])
+        }
+    }
+    
+}
